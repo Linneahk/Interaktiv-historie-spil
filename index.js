c1fc7bcd55cff7dc798a9354b8c19ef297edd1f7
Linneahk <linneahkvernenes@gmail.com>
2024-12-19 14:35:59 +0100

    ja
    ja

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ let heading = document.getElementById("Heading");
 let story = document.getElementById("story");
 let option1 = document.getElementById("Opt1")
 let option2 = document.getElementById("Opt2")
+let option3 = document.getElementById("Opt1.1")
+let option4 = document.getElementById("Opt2.1")
 
 function checkGender(){
-    let boy=document.getElementById("genderBoy").checked;
-    let girl=document.getElementById("genderGirl").checked;
+    let boy =document.getElementById("genderBoy").checked;
+    let girl =document.getElementById("genderGirl").checked;
     let gender = document.getElementById("genderBoks");
     
    
@@ -23,21 +25,24 @@ function setHeading() {
     nameBoks.style.display="none";
     nameText.style.display="none";
     nextButton.style.display="block";
-    nextButton.onclick=start;
+    nextButton.onclick=setHeading;
     heading.innerHTML = "velkommen, " + Firstname.value;
     heading.style.color = "white";
-    option1.style.display = "block"
-    option2.style.display = "block"
+    option1.style.display = "block";
+    option2.style.display = "block";
+    option3.style.display = "block";
+    option4.style.display = "block";
     
    
 }
 function start() {
-heading.innerText="du våkner av ein lyd utfor huset ditt midt på natta";
-story.innerText="kva velger du å gjer";
-nextButton.onclick=nextStep;
-option1.style.display = "none"
-    option2.style.display = "none"
+    heading.innerText="du våkner av ein lyd utfor huset ditt midt på natta";
+    story.innerText="kva velger du å gjer";
+    Opt1.onclick=nextStep;
+    Opt2.onclick=nextStep;
+    nextButton.onclick=nextStep;
 }
+
 function nextStep() {
     heading.innerText="du ser gjennom vinduet og ser ein man springe vekk fra huset ditt.";
     story.innerText="vil du velge å...";
@@ -45,18 +50,14 @@ function nextStep() {
 }
 
 function nextStep1() {
-    heading.innerText="du ser gjennom vinduet og ser ein man springe vekk fra huset ditt.";
-    story.innerText=" vil du velge å...";
+    heading.innerText="han springer rundt hjørnet og forsvinner";
+    story.innerText="du gjør da..";
     nextButton.onclick=nextStep2;
 }
 
-
-
-
-
 function nextStep2() {
-    heading.innerText="du ser gjennom vinduet og ser ein man springe vekk fra huset ditt.";
-    story.innerText=" vil du velge å...";
+    heading.innerText="du går på rommet ditt og legger deg til å sove";
+    story.innerText="du gjør da..";
     nextButton.onclick=nextStep3;
 }
 
