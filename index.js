745014ccac3ab93aabf6169cb6a3ba07688d142a
Benjamin420H <benjaminhelland2008@gmail.com>
2024-12-19 08:09:19 +0100

    push

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let nameText = document.getElementById("genderText");
 let nextButton = document.getElementById("nextButton");
 let heading = document.getElementById("Heading");
 let story = document.getElementById("story");
+let option1 = document.getElementById("Opt1")
+let option2 = document.getElementById("Opt2")
+
 function checkGender(){
     let boy=document.getElementById("genderBoy").checked;
     let girl=document.getElementById("genderGirl").checked;
@@ -23,18 +26,21 @@ function setHeading() {
     nextButton.onclick=start;
     heading.innerHTML = "velkommen, " + Firstname.value;
     heading.style.color = "white";
+    option1.style.display = "block"
+    option2.style.display = "block"
+    
    
 }
 function start() {
-heading.innerText="Overskrift";
-story.innerText="Detter er en test";
+heading.innerText="du våkner av ein lyd utfor huset ditt midt på natta";
+story.innerText="kva velger du å gjer";
 nextButton.onclick=nextStep;
-
+option1.style.display = "none"
+    option2.style.display = "none"
 }
-
 function nextStep() {
-    heading.innerText="du våkner av ein lyd utfor huset ditt midt på natta";
-    story.innerText="kva velger du å gjer";
+    heading.innerText="du ser gjennom vinduet og ser ein man springe vekk fra huset ditt.";
+    story.innerText="vil du velge å...";
     nextButton.onclick=nextStep1;
 }
 
